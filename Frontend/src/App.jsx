8f4d95687cc5d93fc9b5b14b02753d096e67e609
istekhar <istekhararkam25@gmail.com>
2026-01-01 09:15:20 +0530

    Implement main application layout with routing and add core components

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import Sidebar from "./Components/Sidebar.jsx";
+import { Routes, Route } from "react-router-dom";
+import ChatBox from "./Components/ChatBox.jsx";
+import Credits from "./Components/Credits.jsx";
+import Community from "./Components/Community.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="dark:bg-linear-to-b from-[#242124] to-[#000000]">
+        <div className="flex h-screen w-screen ">
+          <Sidebar />
+          <Routes>
+            <Route path="/" element={<ChatBox />} />
+            <Route path="/credits" element={<Credits />} />
+            <Route path="/community" element={<Community />} />
+          </Routes>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
